a34c1d19816dbef596136ac8618fd0262b29ca33
hongbeomi <dks3963@naver.com>
2020-06-27 17:29:44 +0900

    시저암호 kotlin 풀이

---
+++ hongbeom/level1/시저암호/Solution.kt
@@ -0,0 +1,13 @@
+package level1.시저암호
+
+class Solution {
+
+    fun solution(s: String, n: Int) = s.map {
+        when (it) {
+            in ('a'..'z') -> 'a' + ((it - 'a' + n) % 26)
+            in ('A'..'Z') -> 'A' + ((it - 'A' + n) % 26)
+            else -> it
+        }
+    }.joinToString("")
+
+}
