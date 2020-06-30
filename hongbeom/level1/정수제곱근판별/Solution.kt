0ba8fa9d4d04ddb9046701af060253bd9cd22144
hongbeomi <dks3963@naver.com>
2020-06-30 13:23:54 +0900

    정수 제곱근 판별 kotlin 풀이

---
+++ hongbeom/level1/정수제곱근판별/Solution.kt
@@ -0,0 +1,11 @@
+package level1.정수제곱근판별
+
+import kotlin.math.sqrt
+
+class Solution {
+
+    fun solution(n: Long) = sqrt(n.toDouble()).toLong()
+            .takeIf { it * it == n }
+            ?.let { (it + 1) * (it + 1) } ?: -1
+
+}
